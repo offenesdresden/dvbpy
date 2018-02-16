f1047fbcaa3a01f9aa2c5e2c3fec037dbb4dd6df
Kilian Koeltzsch <me@kilian.io>
2018-02-16 19:47:14 +0100

    add transport mode type

---
+++ dvb/mode.py
@@ -0,0 +1,9 @@
+class Mode:
+    TRAM = 'Tram'
+    CITY_BUS = 'CityBus'
+    INTERCITY_BUS = 'IntercityBus'
+    SUBURBAN_RAILWAY = 'SuburbanRailway'
+    TRAIN = 'Train'
+    CABLEWAY = 'Cableway'
+    FERRY = 'Ferry'
+    HAILED_SHARED_TAXI = 'HailedSharedTaxi'
