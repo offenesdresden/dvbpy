fa3dafbb9c0b1f53065b91a221419796bd00dc5d
Kilian Koeltzsch <me@kilian.io>
2018-02-16 13:33:48 +0100

    add python 3.6 version tag
    I think that's what this is? ¯\_(ツ)_/¯

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ bus- and tramstop data in python',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     keywords='dvb vvo tram bus public transport dresden',
