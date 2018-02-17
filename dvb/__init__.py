94ac0586055cc9d6bfaa8ab9b6f296172cf8b687
Kilian Koeltzsch <me@kilian.io>
2018-02-17 16:35:41 +0100

    export relevant public stuff at module level

--- dvb/__init__.py
+++ dvb/__init__.py
@@ -1,5 +1,9 @@
 from .dvb import *
 
+from .stop import *
+from .departure import *
+from .mode import *
+
 """
 An unofficial python module giving you a few options to query a
 collection of publicly accessible API methods for Dresden's public
