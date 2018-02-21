4baf0d8aa2d57184dc45c37973c39359535708e2
Kilian Koeltzsch <me@kilian.io>
2018-02-21 11:53:30 +0100

    add content-type header to all requests

--- dvb/network.py
+++ dvb/network.py
@@ -4,5 +4,5 @@ import requests
 
 
 def post(url: str, params: dict = None) -> dict:
-    r = requests.post(url, params=params)
+    r = requests.post(url, params=params, headers={'Content-Type': 'application/json;charset=UTF-8'})
     return json.loads(r.text)
