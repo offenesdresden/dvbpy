488433568acf0ef9e505a955f193332db0485fb1
Kilian Koeltzsch <me@kilian.io>
2018-02-21 12:26:25 +0100

    use jsonbase in departure

--- dvb/departure.py
+++ dvb/departure.py
@@ -1,19 +1,17 @@
 from datetime import datetime
 
+from .JSONBase import JSONBase
 from .diva import Diva
 from .mot_type import MotType
 from .network import post
 from .util.date import sap_date_to_datetime
 
 
-class Departure:
+class Departure(JSONBase):
     class State:
         IN_TIME = 'InTime'
         DELAYED = 'Delayed'
         # CANCELLED = '?'
-
-    def __init__(self, _dict):
-        self._dict = _dict
 
     def __repr__(self):
         return '{} {} in {}'.format(self.line_name, self.direction, self.fancy_eta())
